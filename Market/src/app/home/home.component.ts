200e72ea1ad35d5855baed9fbf2093d2d370e49f
saraxc <91764673+saraxc@users.noreply.github.com>
2021-12-14 12:15:06 -0800

    home.component.ts

---
+++ Market/src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { AppService } from '../app.service';
+import { NotifierService } from '../notifier.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  cards = [];
+  cardsForHandset = [];
+  cardsForWeb = [];
+
+  isHandset: boolean = false;
+  isHandsetObserver: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return true;
+      }
+      return false;
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver,
+    public appService: AppService,
+    private notifierService: NotifierService) { }
+
+  ngOnInit() {
+    this.isHandsetObserver.subscribe(currentObserverValue => {
+      this.isHandset = currentObserverValue;
+      this.loadCards();
+    });
+
+    this.appService.getDeals().subscribe(
+      response => {
+        this.cardsForHandset = response.handsetCards;
+        this.cardsForWeb = response.webCards;
+        this.loadCards();
+        this.notifierService.showNotification('Todays deals loaded successfully. Click on any deal!', 'OK', 'success');
+      },
+      error => {
+        this.notifierService.showNotification('There was an error in receiving data from server!', 'OK', 'error');
+      }
+    );
+  }
+
+  loadCards() {
+    this.cards = this.isHandset ? this.cardsForHandset : this.cardsForWeb;
+  }
+
+  getImage(imageName: string): string {
+    return 'url(' + 'http://localhost:3000/images/' + imageName + '.jpg' + ')';
+  }
+}
